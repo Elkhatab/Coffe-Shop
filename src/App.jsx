455f52664cd641be7ca70134785ad368f4681c06
Bassam <Elkatab98@icloud.com>
2024-08-21 15:31:49 +0300

    Handle Some Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+
+// Css
 import "./App.css";
-import Header from "./components/Header/Header";
+
+// Components
 import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Features from "./components/Features/Features";
 import Menu from "./components/Menu/Menu";
-import Choose from "./components/Choose/Choose";
-import Stats from "./components/Stats/Stats";
-import Gallery from "./components/Gallery/Gallery";
-import Offer from "./components/Offer/Offer";
 import Team from "./components/Team/Team";
+import About from "./components/About/About";
+import Stats from "./components/Stats/Stats";
+import Offer from "./components/Offer/Offer";
+import Header from "./components/Header/Header";
+import Choose from "./components/Choose/Choose";
+import Gallery from "./components/Gallery/Gallery";
+import Features from "./components/Features/Features";
+
 function App() {
   return (
     <>
